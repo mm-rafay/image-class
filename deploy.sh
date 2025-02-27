bec6cbd8a0344770da40fe2675a55bf76ad195b3
mm-rafay <matt.morris@rafay.co>
2025-02-27 11:44:29 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,18 @@ set -e
 NAMESPACE="ship-detect"
 
 echo "Applying Kubernetes manifests..."
-sudo kubectl apply -f k8s-namespace-pvc.yaml      # contains Namespace and PVCs
+sudo kubectl apply -f ns.yaml 
+sudo kubectl apply -f data-pvc.yaml
+sudo kubectl apply -f model-pvc.yaml
 sudo kubectl apply -f k8s-train-job.yaml          # Training Job manifest
 echo "Waiting for training job to complete..."
 # Optionally, wait for job completion
 sudo kubectl wait --for=condition=complete --timeout=1h job/ship-train-job -n $NAMESPACE
 
 echo "Training job completed. Deploying inference service..."
-sudo kubectl apply -f k8s-inference-deployment.yaml
-sudo kubectl apply -f k8s-inference-service.yaml
+sudo kubectl apply -f inference-dp.yaml
+sudo kubectl apply -f inference-svc.yamlhpa.yaml
+sudo kubectl apply -f hpa.yaml
 
 echo "Deployment initiated. You can monitor pods with 'kubectl get pods -n $NAMESPACE'."
 echo "Once the LOAD BALANCER is ready, use 'kubectl get svc -n $NAMESPACE' to get the external IP/URL."
