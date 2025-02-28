50ddb739dfcda59082f08dac9a82f42d1a5ec6b6
mm-rafay <matt.morris@rafay.co>
2025-02-27 22:34:49 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 sudo kubectl delete pvc ship-model-pvc -n ship-detect
 sudo kubectl delete pvc ship-data-pvc -n ship-detect
 sudo kubectl delete job ship-train-job -n ship-detect
+sudo kubectl delete ns ship-detect
