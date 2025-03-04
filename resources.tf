feeeea4c3c602ec95d60e1fb6f634f7969089094
mm-rafay <matt.morris@rafay.co>
2025-03-04 14:26:42 -0600

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -3,3 +3,157 @@ resource "null_resource" "deploy" {
     command = "echo Deploying..."
   }
 }
+
+/* resource "rafay_workload" "ns" {
+  metadata {
+    name    = "ns"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://ns.yaml"
+        }
+      }
+    }
+  }
+}
+
+resource "rafay_workload" "data-pvc" {
+  metadata {
+    name    = "data-pvc"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://data-pvc.yaml"
+        }
+      }
+    }
+  }
+}
+
+resource "rafay_workload" "model-pvc" {
+  metadata {
+    name    = "model-pvc"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://model-pvc.yaml"
+        }
+      }
+    }
+  }
+}
+
+resource "rafay_workload" "job" {
+  metadata {
+    name    = "job"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://job.yaml"
+        }
+      }
+    }
+  }
+} */
+
+resource "rafay_workload" "inference-dp" {
+  metadata {
+    name    = "inference-dp"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://inference-dp.yaml"
+        }
+      }
+    }
+  }
+}
+
+resource "rafay_workload" "inference-svc" {
+  metadata {
+    name    = "inference-svc"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://inference-svc.yaml"
+        }
+      }
+    }
+  }
+}
+
+resource "rafay_workload" "hpa" {
+  metadata {
+    name    = "hpa"
+    project = "gpu-paas-demo"
+  }
+  spec {
+    namespace = "default"
+    placement {
+      selector = "rafay.dev/clusterName=mm-gpu"
+    }
+    version = "v0"
+    artifact {
+      type = "Yaml"
+      artifact {
+        paths {
+          name = "file://hpa.yaml"
+        }
+      }
+    }
+  }
+}
